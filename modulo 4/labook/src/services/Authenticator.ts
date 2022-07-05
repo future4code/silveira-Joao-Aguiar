6775dc26d87c0a27c588cba65af0fe52ed7ddaad
jmaraguiar <jmaraguiar22@gmail.com>
2022-07-05 18:38:06 -0300

    inicio labook => signup e base para o login

---
+++ modulo 4/labook/src/services/Authenticator.ts
@@ -0,0 +1,25 @@
+import * as jwt from "jsonwebtoken"
+
+export interface AuthenticationData {
+    id: string
+}
+
+export class Authenticator {
+
+    public generateToken(input: AuthenticationData): string{
+        const token = jwt.sign(
+            input,
+            String(process.env.JWT_KEY),
+            {
+                expiresIn: process.env.AUTH_EXPIRES_IN
+            }
+        )
+
+        return token
+    }
+
+    public getTokenData(token: string): AuthenticationData{
+        const data = jwt.verify(token, String(process.env.JWT_KEY))
+        return data as AuthenticationData
+    }
+}
