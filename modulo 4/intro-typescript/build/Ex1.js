477ddcab9240dce63fa02c24ef751e92242d1380
jmaraguiar <jmaraguiar22@gmail.com>
2022-07-25 17:44:16 -0300

    Promobit Case 1

---
+++ modulo 4/intro-typescript/build/Ex1.js
@@ -0,0 +1,13 @@
+function checaTriangulo(a, b, c) {
+    if (a !== b && b !== c) {
+        return "Escaleno";
+    }
+    else if (a === b && b === c) {
+        return "Equilátero";
+    }
+    else {
+        return "Isósceles";
+    }
+}
+console.log(checaTriangulo(2, 4, 7));
+//# sourceMappingURL=Ex1.js.map
---
+++ modulo 4/intro-typescript/build/Ex5.js
@@ -0,0 +1,13 @@
+function checaRenovacaoRG() {
+    const anoAtual = Number(process.argv[2]);
+    const anoNascimento = Number(prompt(process.argv[3]));
+    const anoEmissao = Number(prompt(process.argv[4]));
+    const idade = anoAtual - anoNascimento;
+    const tempoCarteira = anoAtual - anoEmissao;
+    const cond1 = idade <= 20 && tempoCarteira >= 5;
+    const cond2 = idade > 20 && idade <= 50 && tempoCarteira >= 10;
+    const cond3 = idade > 50 && tempoCarteira >= 15;
+    return (cond1 || cond2 || cond3);
+}
+console.log(checaRenovacaoRG());
+//# sourceMappingURL=Ex5.js.map
