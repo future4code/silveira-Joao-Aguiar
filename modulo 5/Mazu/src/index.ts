164a76fea46255f2006edcbab2c919d930d798ea
jmaraguiar <jmaraguiar22@gmail.com>
2022-08-01 04:26:51 -0300

    coreções

--- modulo 5/Mazu/src/index.ts
+++ modulo 5/Mazu/src/index.ts
@@ -1,10 +1,7 @@
 import { app } from "./app";
 import { dogWalkingRoute } from "./controllers/routes/DogWalkingRouter";
-import { indexRoute } from "./controllers/routes/IndexRouter";
 import { userRoute } from "./controllers/routes/UserRouter";
 
 app.use("/walk", dogWalkingRoute)
-
-app.use("/index", indexRoute)
 
 app.use("/user", userRoute)
